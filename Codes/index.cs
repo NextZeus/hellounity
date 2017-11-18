c00e3bd8f228255dbf9ea913296b4960da9fc16f
NextZeus <875697126@qq.com>
2017-11-18 08:09:26 +0800

    invoke

--- Codes/index.cs
+++ Codes/index.cs
@@ -116,3 +116,34 @@ public class UsingInstantiate: MonoBehaviour
         }
     }
 }
+
+public class InvokeScript: MonoBehaviour
+{
+    public GameObject target;
+
+    void Start(){
+        Invoke('SpawnObject',2); //call SpawnObject function after 2s
+    }
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0,2,0), Quaternion.identity);
+    }
+}
+
+public class InvokeRepeating : MonoBehaviour 
+{
+    public GameObject target;
+    
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+    }
+    
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+    }
+}
